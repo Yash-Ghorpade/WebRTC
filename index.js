4ca125be57fd21ce5a82a04422a074210c7652ce
Yash Ghorpade <yashmayughorpade@gmail.com>
2025-06-15 12:43:45 +0530

    re written server

--- index.js
+++ index.js
@@ -1,34 +1,38 @@
-const express = require("express");
-const http = require("http");
-const cors = require("cors");
 const { Server } = require("socket.io");
 
-const app = express();
-app.use(cors());
-
-const server = http.createServer(app);
-
-const io = new Server(server, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"]
-  }
+const io = new Server(8080, {
+  cors: true,
 });
 
-io.on("connection", socket => {
-  socket.emit("me", socket.id);
+const emailToSocketIdMap = new Map();
+const socketidToEmailMap = new Map();
 
-  socket.on("disconnect", () => {
-    socket.broadcast.emit("callEnded");
+io.on("connection", (socket) => {
+  console.log(`Socket Connected`, socket.id);
+  socket.on("room:join", (data) => {
+    const { email, room } = data;
+    emailToSocketIdMap.set(email, socket.id);
+    socketidToEmailMap.set(socket.id, email);
+    io.to(room).emit("user:joined", { email, id: socket.id });
+    socket.join(room);
+    io.to(socket.id).emit("room:join", data);
   });
 
-  socket.on("callUser", ({ userToCall, signalData, from, name }) => {
-    io.to(userToCall).emit("callUser", { signal: signalData, from, name });
+  socket.on("user:call", ({ to, offer }) => {
+    io.to(to).emit("incomming:call", { from: socket.id, offer });
   });
 
-  socket.on("answerCall", (data) => {
-    io.to(data.to).emit("callAccepted", data.signal);
+  socket.on("call:accepted", ({ to, ans }) => {
+    io.to(to).emit("call:accepted", { from: socket.id, ans });
+  });
+
+  socket.on("peer:nego:needed", ({ to, offer }) => {
+    console.log("peer:nego:needed", offer);
+    io.to(to).emit("peer:nego:needed", { from: socket.id, offer });
+  });
+
+  socket.on("peer:nego:done", ({ to, ans }) => {
+    console.log("peer:nego:done", ans);
+    io.to(to).emit("peer:nego:final", { from: socket.id, ans });
   });
 });
-
-server.listen(8080, () => console.log("Signaling server running on port 8080"));
